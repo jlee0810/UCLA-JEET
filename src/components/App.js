52bfde7a9b2eee5032a678a96e7aa9aac6ca0839
Joonwon (Eric) Lee <joonwontech@gmail.com>
2022-06-14 17:40:54 +0900

    Progress Until Log Out

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import AppRouter from "components/Router";
+import { useState, useEffect } from "react";
+import { authService } from "fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        const uid = user.uid;
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Loading..."}
+      <footer>&copy; UCLA JEET {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Profile.js
@@ -0,0 +1,18 @@
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
+export default Profile;
